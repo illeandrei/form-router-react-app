854da745602dc5cf5e6409b0a7861230daa8aa95
Andrei Ille <aille@sdl.com>
2018-04-02 15:50:12 +0300

    - added routes (via react-router) - added new post form (via redux-form)

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,16 +1,30 @@
 import axios from 'axios';
 
 export const FETCH_POSTS = 'fetch_posts';
+export const CREATE_POSTS = 'create_posts';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
-// const API_KEY = '?key=aille123';
-const API_KEY = '?key=123';
+const API_KEY = '?key=aille123';
 
 export function fetchPosts() {
     const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
 
     return {
         type: FETCH_POSTS,
+        payload: request
+    }
+}
+
+export function createPost(values, callback) {
+    //we need to make a post request to get an id for this post object,
+    // rather then to just pass the values object to the state
+    const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, values)
+        .then(() => {
+            callback();
+        });
+
+    return {
+        type: CREATE_POSTS,
         payload: request
     }
 }
--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,6 +1,6 @@
 import { FETCH_POSTS } from '../actions/index';
 
-export default function (state = [], action) {
+export default function (state = {}, action) {
     switch (action.type) {
         case FETCH_POSTS:
             let posts = {};
