9f341645acee8840b71c580b587945c123f5166d
Andrei Ille <aille@sdl.com>
2018-04-03 10:08:41 +0300

    -added delete post reducer

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,7 +1,12 @@
-import { FETCH_POSTS, FETCH_POST } from '../actions/index';
+import _ from 'lodash';
+
+import { FETCH_POSTS, FETCH_POST, DELETE_POSTS } from '../actions/index';
 
 export default function (state = {}, action) {
     switch (action.type) {
+        case DELETE_POSTS:
+            return _.omit(state, action.payload);
+
         case FETCH_POST:
             // const post = action.payload.data;
             // const newState = {...state};
